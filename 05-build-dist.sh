b484bf02561c9ade3fa9155734b959cd27a070e4
Alexander Theißen <alex.theissen@me.com>
2023-09-12 14:54:42 +0200

    Pass target when creating dist

--- 05-build-dist.sh
+++ 05-build-dist.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+source config.sh
+
 cd rust
 mkdir -p build/riscv32em-unknown-none-elf/compiler-doc
-./x dist
+./x dist --target $TOOLCHAIN_HOST_TRIPLET --target riscv32em-unknown-none-elf
